c52a10fa66083677db003ee28602051a9ff0cc35
fiatjaf <fiatjaf@gmail.com>
2022-03-09 16:04:41 -0300

    make it so it is buildable by netlify.

--- build.js
+++ build.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 
-require('esbuild').buildSync({
-  entryPoints: ['globals.js'],
-  outfile: 'globals.bundle.js',
-  bundle: true
-})
+const esbuild = require('esbuild')
+
+const prod = process.argv.indexOf('prod') !== -1
+
+esbuild
+  .build({
+    entryPoints: ['globals.js'],
+    outfile: 'globals.bundle.js',
+    bundle: true,
+    sourcemap: prod ? false : 'inline',
+    minify: prod
+  })
+  .then(() => console.log('build success.'))
